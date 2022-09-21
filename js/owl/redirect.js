38f5bc841898ffd92f1bcd717f2c7f60e06d4187
leandrogarciadev <leandrogarcia.dev@gmail.com>
2022-09-21 19:32:56 -0300

    Primeiro commit

---
+++ js/owl/redirect.js
@@ -0,0 +1,23 @@
+
+function autor(){
+   // window.open("https://dio.me"); // Abrir uma nova aba do browser
+    window.location.href = "https://github.com/leandrogarciadev"; // abrir o link na mesma página
+}
+function documentarios(){
+    window.location.href = "https://www.netflix.com/br/browse/genre/6839";
+}
+function filmes(){
+    window.location.href = "https://www.netflix.com/br/browse/genre/34399"; 
+}
+function series(){
+    window.location.href = "https://www.netflix.com/br/browse/genre/83";
+}
+function inicio(){
+    alert("Você está na página principal!")
+}
+function informacoes(){
+    window.location.href = "https://www.netflix.com/br/title/81002370";
+}
+function assistir(){
+    window.location.href = "https://youtu.be/gfDTor2B5ck?t=9";
+}
